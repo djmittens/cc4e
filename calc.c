7eabf76ecb43d080a257db110f8153f9199a1b46
Nikita Gridinskiy <djmittens@users.noreply.github.com>
2023-05-29 21:01:02 -0700

    Initial commit

---
+++ calc.c
@@ -0,0 +1,44 @@
+#include <stdio.h>
+#include <string.h>
+#include <stdlib.h>
+
+int main()
+{
+    char line[256];
+    char opcode;
+    float value, display = 0.0;
+
+    FILE *handle = fopen("../calc.input.txt", "r");
+
+    while (fgets(line, 256, handle) != NULL)
+    {
+        // Use sscanf to parse data from a string
+        sscanf(line, "%c %f", &opcode, &value);
+
+        /* Replace this with your code */
+        switch (opcode)
+        {
+        case '=':
+            display = value;
+            break;
+        case '*':
+            display *= value;
+            break;
+        case '+':
+            display += value;
+            break;
+        case '-':
+            display -= value;
+            break;
+        case '/':
+            display /= value;
+            break;
+        case 'S':
+            return EXIT_SUCCESS;
+        }
+
+        printf("Display: %.2f\n", display);
+    }
+
+}
+ 
---
+++ uniq.c
@@ -0,0 +1,27 @@
+#include <stdio.h>
+#include <string.h>
+#include <stdlib.h>
+
+int main()
+{
+    char line[1000];
+    char keep[1000];
+
+    FILE *handle = fopen("../uniq.input.txt", "r");
+
+    if (handle == NULL)
+    {
+        return EXIT_FAILURE;
+    }
+
+    while (fgets(line, 1000, handle) != NULL)
+    {
+        if (strcmp(line, keep) != 0)
+        {
+            strcpy(keep, line);
+            printf("%s", keep);
+        }
+    }
+
+    fclose(handle); // hah always close
+}
