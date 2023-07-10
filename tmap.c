16fc2e1b56a2227e4d08d384c92f0a18df35c9aa
Nikita Gridinskiy <djmittens@users.noreply.github.com>
2023-07-10 00:45:03 -0700

    Adding property testing for trees

--- tmap.c
+++ tmap.c
@@ -3,6 +3,7 @@
 #include <stdlib.h>
 #include <string.h>
 #include <stdint.h>
+#include <math.h>
 
 const uint8_t TreeMap_Entry_BLACK = 0;
 const uint8_t TreeMap_Entry_RED = 1;
@@ -120,7 +121,7 @@ void __TreeMap_insertNode(PtrTreeMap_Entry root, PtrTreeMap_Entry node)
     }
 }
 
-PtrTreeMap_Entry __TreeMapEntry_traverse(
+PtrTreeMap_Entry __TreeMapEntry_find(
     PtrTreeMap_Entry self,
     int depth,
     uint8_t isBfs,
@@ -129,24 +130,24 @@ PtrTreeMap_Entry __TreeMapEntry_traverse(
     if (self == NULL)
         return NULL;
 
-    if (isBfs)
+    if (isBfs && visit(self, depth))
     {
-        visit(self, depth);
+        return self;
     }
 
     PtrTreeMap_Entry res = NULL;
     if (self->__left != NULL)
     {
-        res = __TreeMapEntry_traverse(self->__left, depth + 1, isBfs, visit);
+        res = __TreeMapEntry_find(self->__left, depth + 1, isBfs, visit);
     }
-    if (self->__right != NULL)
+    if (res == NULL && self->__right != NULL)
     {
-        res = __TreeMapEntry_traverse(self->__right, depth + 1, isBfs, visit);
+        res = __TreeMapEntry_find(self->__right, depth + 1, isBfs, visit);
     }
 
-    if (!isBfs)
+    if (!isBfs && visit(self, depth))
     {
-        visit(self, depth);
+        return self;
     }
 
     return res;
@@ -213,7 +214,7 @@ void _dump_tree(PtrTreeMap_Entry node)
         root = root->__parent;
     }
     // print tree
-    __TreeMapEntry_traverse(root, 0, 1, __TreeMap_dump_node);
+    __TreeMapEntry_find(root, 0, 1, __TreeMap_dump_node);
 }
 
 void _fix_node(PtrTreeMap_Entry node)
@@ -230,8 +231,19 @@ void _fix_node(PtrTreeMap_Entry node)
     }
     printf("Tree BEFORE >>>>>>>>>>>>>>>>\n");
     _dump_tree(node);
-    // If uncle is black we recolor
-    // if uncle is red we rebalance
+    // If uncle is red we recolor
+    if (ucolor == TreeMap_Entry_RED)
+    {
+        node->__color = TreeMap_Entry_RED;
+        node->__parent->__color = TreeMap_Entry_BLACK;
+        uncle->__color = TreeMap_Entry_BLACK;
+        // if grandpa isnt root set it red
+        if (uncle->__parent->__parent != NULL)
+        {
+            uncle->__parent->__color = TreeMap_Entry_RED;
+        }
+    }
+    // if uncle is black we rebalance
     printf("Tree AFTER >>>>>>>>>>>>>>>>\n");
     _dump_tree(node);
 }
@@ -288,6 +300,7 @@ void __TreeMap_put(PtrTreeMap self, char *key, int value)
             __TreeMap_insertNode(self->__root, new);
             balance(new);
         }
+        self->__count++;
     }
     else
     {
@@ -318,24 +331,119 @@ PtrTreeMap_Iter __TreeMap_iter(struct TreeMap *self)
     return res;
 }
 
+////////////////// TESTS //////////////////////////
+int __TreeMap_test_root_is_black(PtrTreeMap self)
+{
+    return self->__root == NULL || self->__root->__color == TreeMap_Entry_BLACK;
+}
+
+int __TreeMap_find_red_red_node(PtrTreeMap_Entry self, int depth)
+{
+    if (self != NULL && self->__color == TreeMap_Entry_RED)
+    {
+        if (self->__left != NULL && self->__left->__color == TreeMap_Entry_RED)
+        {
+            printf("[ERROR]: Node { %s } violates [Property 2]\n", self->__left->key);
+            return 1;
+        }
+        if (self->__right != NULL && self->__right->__color == TreeMap_Entry_RED)
+        {
+            printf("[ERROR]: Node { %s } violates [Property 2]\n", self->__right->key);
+            return 1;
+        }
+    }
+    return 0;
+}
+int __TreeMap_test_no_red_node_red_child(PtrTreeMap self)
+{
+    PtrTreeMap_Entry res = __TreeMapEntry_find(self->__root, 0, 0, __TreeMap_find_red_red_node);
+    return res == NULL;
+}
+
+int __TreeMap_test_each_path_has_same_num_black_nodes(PtrTreeMap self)
+{
+    // Reset counts just for now
+    // This forumalation, of this algorithm, could potentially be implemented with find
+    // and globals, however it was fun to write this implementation anyway
+    int pass = 1;
+    int max_height = (int)log2((double)self->__count) + 2;
+    PtrTreeMap_Entry *path_buf = malloc(sizeof(*path_buf) * max_height);
+    PtrTreeMap_Entry *head = path_buf;
+    *path_buf = self->__root;
+    
+    int blacks_per_path = 0;
+    while (head >= path_buf)
+    {
+        // pop it
+        PtrTreeMap_Entry node = *head;
+        head--;
+        // push it
+        if (node->__right != NULL)
+        {
+            head++;
+            *head = node->__right;
+        }
+        if (node->__left != NULL)
+        {
+            head++;
+            *head = node->__left;
+        }
+        // finish it
+        if (node->__right == NULL && node->__left == NULL)
+        {
+            int black_counter = 0;
+            PtrTreeMap_Entry h = node;
+            while (h != NULL)
+            {
+                if (h->__color == TreeMap_Entry_BLACK)
+                {
+                    black_counter ++;
+                }
+                h = h->__parent;
+            }
+            printf("Leaf found %s #[%d]\n", node->key, black_counter);
+            if(!blacks_per_path) {
+                blacks_per_path = black_counter;
+            } else if (blacks_per_path != black_counter) {
+                pass = 0;
+            }
+        }
+    }
+    free(path_buf);
+    return pass;
+}
+
+void __TreeMap_test_tree(PtrTreeMap self)
+{
+    printf("[Property 1] Root is always black .......................... %s\n",
+           __TreeMap_test_root_is_black(self) ? "[PASS]" : "[FAIL]");
+    printf("[Property 2] No red node has a red child ................... %s\n",
+           __TreeMap_test_no_red_node_red_child(self) ? "[PASS]" : "[FAIL]");
+    printf("[Property 3] Every path has same number of black nodes ..... %s\n",
+           __TreeMap_test_each_path_has_same_num_black_nodes(self) ? "[PASS]" : "[FAIL]");
+}
+////////////////// END TESTS //////////////////////
+
 int main(void)
 {
     printf("Testing TreeMap\n");
 
+    srand(12345678);
     PtrTreeMap map = TreeMap_new();
     if (map)
     {
-        map->put(map, "h", 42);
-        map->put(map, "d", 8);
-        // map->put(map, "f", 5);
-        map->put(map, "b", 123);
-        map->put(map, "k", 9);
-        map->put(map, "m", 67);
-        map->put(map, "j", 12);
-        map->put(map, "f", 6);
-
-        map->dump(map);
+        for (int i = 0; i < 20; ++i)
+        {
+            int k = rand();
+            char v[2];
+            v[0] = 'a' + (k % 26); // random character
+            map->put(map, v, k);
+        }
     }
+    printf("\n\n\n\nRESULT:\n");
+    map->dump(map);
+    printf("\n\n");
+    __TreeMap_test_tree(map);
     // map->free(map);
     return EXIT_SUCCESS;
 }
